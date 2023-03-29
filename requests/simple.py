2b16e32df198ff50a4feba968e7e2139dfab65e5
raybai <raybai@synology.com>
2023-03-29 11:53:24 +0800

    update request/

--- requests/simple.py
+++ requests/simple.py
@@ -1,7 +1,12 @@
 import requests
 
-res = requests.get('http://www.example.com', cookies={'key':'value'})
-res = requests.post('http://www.example.com', data={'key':'value'})
+try:
+    res = requests.get('http://www.example.com', cookies={'key':'value'})
+    res = requests.post('http://www.aexample.com', data={'key':'value'})
+    res.raise_for_status()
+except Exception as err:
+    print(f'HTTP error occurred: {err}')
+
 print(res.text)
 
 
